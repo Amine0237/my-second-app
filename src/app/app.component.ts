4e84161294cfd8e15325bc9fdfd5b8f1be7a103e
Amine0237 <chadli.medamin@gmail.com>
2024-07-04 22:58:25 +0100

    yes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-second-app';
+export class AppComponent implements OnInit{
+
+  users: any;
+  url = "https://api.github.com/users";
+  isLoading = false;
+
+  constructor(private http: HttpClient){}
+  
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get(this.url).subscribe(res => {
+      
+      this.isLoading = true;
+      
+      setTimeout(()=>{
+        this.users = res;
+        this.isLoading = false
+      }, 3000);
+      console.log(res);
+    }, error => {
+      console.log(error);
+    }, () => {
+      console.log('completed');
+    });
+    
+  }
+  
+  goToGithub(url:string){
+    window.open(url, '_blank');
+  }
+  
+  
 }

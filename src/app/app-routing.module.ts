2b7d20b6f8f97c5cb785a3953de74ecdcb285d1b
Amine0237 <chadli.medamin@gmail.com>
2024-07-04 12:09:23 +0100

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent}, 
+  {path: 'products', component: ProductsComponent}, 
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', redirectTo: 'home'} // you can also display an error component
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  constructor(private router: Router){
+    
+  }
+
+  goToProducts(){
+    console.log("trying to update db");
+    setTimeout(() => {
+      console.log("Database is updated");
+      this.router.navigate(['products']);
+    }, 2000)
+  }
+
+}
